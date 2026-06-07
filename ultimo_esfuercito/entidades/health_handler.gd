221d283061ae2d9030ec8327c2e0a4f3ca54e01c
Sasukoso <sasukoso11@hotmail.com>
2026-06-07 13:15:21 -0300

    agregado ultimo nivel

--- ultimo_esfuercito/entidades/health_handler.gd
+++ ultimo_esfuercito/entidades/health_handler.gd
@@ -5,7 +5,7 @@ signal entity_died
 signal damage_taken
 signal health_restored
 
-const MAX_HP = 100
+@export var max_hp = 100
 
 var current_health = 100:
 	set(value):
@@ -20,8 +20,8 @@ func take_damage(dam:float)->void:
 	damage_taken.emit(current_health)
 
 func restore_health(heal:float)->void:
-	if(current_health+heal>MAX_HP):
-		current_health=MAX_HP
+	if(current_health+heal>max_hp):
+		current_health=max_hp
 	else:
 		current_health=current_health+heal
 	health_restored.emit(current_health)
